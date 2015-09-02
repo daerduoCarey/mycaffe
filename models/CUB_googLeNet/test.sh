22af8d33cbabe206a33ef83152015bdd8bc2e4b4
Kaichun Mo <daerduoCarey@163.com>
2015-09-02 15:36:12 -0400

    mycaffe

--- models/CUB_googLeNet/test.sh
+++ models/CUB_googLeNet/test.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 
-./build/tools/caffe test -model models/CUB_googLeNet/train_test.prototxt -weights models/CUB_googLeNet/caffemodels/CUB_googLeNet_iter_74000.caffemodel -gpu 0 -iterations 91
+./build/tools/caffe test -model models/CUB_googLeNet/train_test.prototxt -weights models/CUB_googLeNet/caffemodels/CUB_googLeNet_iter_14000.caffemodel -gpu 0 -iterations 91
